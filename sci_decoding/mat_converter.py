98e0a59e2eb5dd0aeddfaa68dd3414b56de2356d
doglman <samswindler@gmail.com>
2023-07-03 12:36:17 -0600

    Code + documentation about SCI decoding attempt

---
+++ sci_decoding/mat_converter.py
@@ -0,0 +1,24 @@
+import scipy.io
+from xml_to_dict import XMLtoDict
+from pprint import pprint
+
+parser = XMLtoDict()
+data = scipy.io.loadmat("2023-06-29 OBU message at 5.915.mat")
+
+metadata = data["rsaMetadata"][0]
+metadata = parser.parse(data["rsaMetadata"][0])
+metadata = metadata["DataFile"] # ["Setup"]["RSAPersist"]
+
+
+
+print(metadata.keys())
+
+
+# pprint(metadata)
+
+
+iq = data["Y"].flatten()
+
+# print(iq.dtype)
+
+# iq.tofile("2023-06-29_OBU.cf64")
